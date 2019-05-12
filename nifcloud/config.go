62de5fbd82f976c22bbea9dc1120f8f85bb04bda
alice02 <scene.4913@gmail.com>
2019-05-12 17:32:02 +0900

    Add config struct for each services.
    * To create new Config, it is necessary to AccessKey, SecretKey and Region string.
    * nifcloud.Config is same as aws.Config (field values are copied from github.com/aws/aws-sdk-go-v2/aws.Config)

---
+++ nifcloud/config.go
@@ -0,0 +1,124 @@
+package nifcloud
+
+import (
+	"net/http"
+
+	"github.com/alice02/nifcloud-sdk-go-v2/nifcloud/endpoints"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/defaults"
+)
+
+// A Config provides service configuration for service clients.
+// This struct is copied from github.com/aws/aws-sdk-go-v2/aws.Config
+type Config struct {
+	// The region to send requests to. This parameter is required and must
+	// be configured globally or on a per-client basis unless otherwise
+	// noted. A full list of regions is found in the "Regions and Endpoints"
+	// document.
+	//
+	// See http://docs.aws.amazon.com/general/latest/gr/rande.html for
+	// information on AWS regions.
+	Region string
+
+	// The credentials object to use when signing requests. Defaults to a
+	// chain of credential providers to search for credentials in environment
+	// variables, shared credential file, and EC2 Instance Roles.
+	Credentials aws.CredentialsProvider
+
+	// The resolver to use for looking up endpoints for AWS service clients
+	// to use based on region.
+	EndpointResolver aws.EndpointResolver
+
+	// The HTTP client to use when sending requests. Defaults to
+	// `http.DefaultClient`.
+	HTTPClient *http.Client
+
+	// TODO document
+	Handlers aws.Handlers
+
+	// Retryer guides how HTTP requests should be retried in case of
+	// recoverable failures.
+	//
+	// When nil or the value does not implement the request.Retryer interface,
+	// the client.DefaultRetryer will be used.
+	//
+	// When both Retryer and MaxRetries are non-nil, the former is used and
+	// the latter ignored.
+	//
+	// To set the Retryer field in a type-safe manner and with chaining, use
+	// the request.WithRetryer helper function:
+	//
+	//   cfg := request.WithRetryer(aws.NewConfig(), myRetryer)
+	Retryer aws.Retryer
+
+	// An integer value representing the logging level. The default log level
+	// is zero (LogOff), which represents no logging. To enable logging set
+	// to a LogLevel Value.
+	LogLevel aws.LogLevel
+
+	// The logger writer interface to write logging messages to. Defaults to
+	// standard out.
+	Logger aws.Logger
+
+	// EnforceShouldRetryCheck is used in the AfterRetryHandler to always call
+	// ShouldRetry regardless of whether or not if request.Retryable is set.
+	// This will utilize ShouldRetry method of custom retryers. If EnforceShouldRetryCheck
+	// is not set, then ShouldRetry will only be called if request.Retryable is nil.
+	// Proper handling of the request.Retryable field is important when setting this field.
+	//
+	// TODO this config field is depercated and needs removed.
+	EnforceShouldRetryCheck bool
+
+	// DisableRestProtocolURICleaning will not clean the URL path when making
+	// rest protocol requests.  Will default to false. This would only be used
+	// for empty directory names in s3 requests.
+	//
+	// Example:
+	//    cfg, err := external.LoadDefaultAWSConfig()
+	//    cfg.DisableRestProtocolURICleaning = true
+	//
+	//    svc := s3.New(cfg)
+	//    out, err := svc.GetObject(&s3.GetObjectInput {
+	//    	Bucket: aws.String("bucketname"),
+	//    	Key: aws.String("//foo//bar//moo"),
+	//    })
+	//
+	// TODO need better way of representing support for this concept. Not on Config.
+	DisableRestProtocolURICleaning bool
+
+	// DisableEndpointHostPrefix will disable the SDK's behavior of prefixing
+	// request endpoint hosts with modeled information.
+	//
+	// Disabling this feature is useful when you want to use local endpoints
+	// for testing that do not support the modeled host prefix pattern.
+	DisableEndpointHostPrefix bool
+}
+
+// NewConfig returns a new Config with access key, secret key and region.
+func NewConfig(key, secret, region string) Config {
+	return Config{
+		Region:           region,
+		EndpointResolver: endpoints.NewDefaultResolver(),
+		Credentials:      NewStaticCredentialsProvider(key, secret),
+		HTTPClient:       defaults.HTTPClient(),
+		Logger:           defaults.Logger(),
+		Handlers:         defaults.Handlers(),
+	}
+}
+
+// AWSConfig converts the config values to aws.Config.
+func (c Config) AWSConfig() aws.Config {
+	return aws.Config{
+		Region:                         c.Region,
+		Credentials:                    c.Credentials,
+		EndpointResolver:               c.EndpointResolver,
+		HTTPClient:                     c.HTTPClient,
+		Handlers:                       c.Handlers,
+		Retryer:                        c.Retryer,
+		LogLevel:                       c.LogLevel,
+		Logger:                         c.Logger,
+		EnforceShouldRetryCheck:        c.EnforceShouldRetryCheck,
+		DisableRestProtocolURICleaning: c.DisableRestProtocolURICleaning,
+		DisableEndpointHostPrefix:      c.DisableEndpointHostPrefix,
+	}
+}
---
+++ nifcloud/static_provider.go
@@ -0,0 +1,32 @@
+package nifcloud
+
+import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+)
+
+// A StaticCredentialsProvider is a set of credentials which are set programmatically.
+type StaticCredentialsProvider struct {
+	Value aws.Credentials
+}
+
+// NewStaticCredentialsProvider return a StaticCredentialsProvider initialized with the NIFCLOUD credentials passed in.
+func NewStaticCredentialsProvider(key, secret string) StaticCredentialsProvider {
+	return StaticCredentialsProvider{
+		Value: aws.Credentials{
+			AccessKeyID:     key,
+			SecretAccessKey: secret,
+		},
+	}
+}
+
+// Retrieve returns the credentials or error if the credentials are invalid.
+func (s StaticCredentialsProvider) Retrieve() (aws.Credentials, error) {
+	v := s.Value
+	if v.AccessKeyID == "" || v.SecretAccessKey == "" {
+		return aws.Credentials{}, errors.New("static credentials are empty")
+	}
+
+	return v, nil
+}
