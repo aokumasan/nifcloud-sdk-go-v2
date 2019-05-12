176e374c8defa8e93727efb4a9fbf9299a0336ed
alice02 <scene.4913@gmail.com>
2019-05-12 18:38:41 +0900

    Add more service file modifying parameters for computing

--- private/model/cli/gen-api/main.go
+++ private/model/cli/gen-api/main.go
@@ -283,6 +283,8 @@ func fixServiceFile(code string) string {
 	maps := []map[string]string{
 		{`"github.com/aws/aws-sdk-go-v2/aws"`: `"github.com/aws/aws-sdk-go-v2/aws"
 "github.com/alice02/nifcloud-sdk-go-v2/nifcloud"`},
+		{"github.com/aws/aws-sdk-go-v2/private/protocol/computing": "github.com/alice02/nifcloud-sdk-go-v2/private/protocol/computing"},
+		{"github.com/aws/aws-sdk-go-v2/aws/signer/v2": "github.com/alice02/nifcloud-sdk-go-v2/nifcloud/signer/v2"},
 		{"aws.Config": "nifcloud.Config"},
 		{"config,": "config.AWSConfig(),"},
 	}
