fde8a1265c5a4fcfeb2a3b5b1f7f499224c7ab18
alice02 <scene.4913@gmail.com>
2019-05-12 18:39:14 +0900

    Add protocol for computing

---
+++ private/protocol/computing/build.go
@@ -0,0 +1,49 @@
+// Package computing provides serialization of NIFCLOUD Computing requests and responses.
+package computing
+
+import (
+	"fmt"
+	"net/url"
+	"reflect"
+
+	"github.com/alice02/nifcloud-sdk-go-v2/private/protocol/query/queryutil"
+	request "github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/awserr"
+)
+
+// BuildHandler is a named request handler for building computing protocol requests
+var BuildHandler = request.NamedHandler{Name: "awssdk.computing.Build", Fn: Build}
+
+// Build builds a request for the EC2 protocol.
+func Build(r *request.Request) {
+	body := url.Values{
+		"Action":  {r.Operation.Name},
+		"Version": {r.Metadata.APIVersion},
+	}
+	if err := queryutil.Parse(body, r.Params, true); err != nil {
+		r.Error = awserr.New("SerializationError", "failed encoding NIFCLOUD Computing Query request", err)
+	}
+
+	// Fix request parameters of DescribeLoadBalancers for NIFCLOUD
+	if r.Operation.Name == "DescribeLoadBalancers" {
+		parameterLength := reflect.ValueOf(r.Params).Elem().FieldByName("LoadBalancerNames").Len()
+		prefix := "LoadBalancerNames"
+		for i := 1; i <= parameterLength; i++ {
+			body[fmt.Sprintf("%s.LoadBalancerPort.%d", prefix, i)] = body[fmt.Sprintf("%s.member.%d.LoadBalancerPort", prefix, i)]
+			body[fmt.Sprintf("%s.InstancePort.%d", prefix, i)] = body[fmt.Sprintf("%s.member.%d.InstancePort", prefix, i)]
+			body[fmt.Sprintf("%s.member.%d", prefix, i)] = body[fmt.Sprintf("%s.member.%d.LoadBalancerName", prefix, i)]
+			delete(body, fmt.Sprintf("%s.member.%d.LoadBalancerPort", prefix, i))
+			delete(body, fmt.Sprintf("%s.member.%d.InstancePort", prefix, i))
+			delete(body, fmt.Sprintf("%s.member.%d.LoadBalancerName", prefix, i))
+		}
+	}
+
+	if r.ExpireTime == 0 {
+		r.HTTPRequest.Method = "POST"
+		r.HTTPRequest.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+		r.SetBufferBody([]byte(body.Encode()))
+	} else { // This is a pre-signed request
+		r.HTTPRequest.Method = "GET"
+		r.HTTPRequest.URL.RawQuery = body.Encode()
+	}
+}
---
+++ private/protocol/computing/unmarshal.go
@@ -0,0 +1,59 @@
+package computing
+
+import (
+	"encoding/xml"
+	"io"
+
+	"github.com/alice02/nifcloud-sdk-go-v2/private/protocol/xml/xmlutil"
+	request "github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/awserr"
+)
+
+// UnmarshalHandler is a named request handler for unmarshaling computing protocol requests
+var UnmarshalHandler = request.NamedHandler{Name: "awssdk.computing.Unmarshal", Fn: Unmarshal}
+
+// UnmarshalMetaHandler is a named request handler for unmarshaling computing protocol request metadata
+var UnmarshalMetaHandler = request.NamedHandler{Name: "awssdk.computing.UnmarshalMeta", Fn: UnmarshalMeta}
+
+// UnmarshalErrorHandler is a named request handler for unmarshaling computing protocol request errors
+var UnmarshalErrorHandler = request.NamedHandler{Name: "awssdk.computing.UnmarshalError", Fn: UnmarshalError}
+
+// Unmarshal unmarshals a response body for the EC2 protocol.
+func Unmarshal(r *request.Request) {
+	defer r.HTTPResponse.Body.Close()
+	decoder := xml.NewDecoder(r.HTTPResponse.Body)
+	err := xmlutil.UnmarshalXML(r.Data, decoder, "")
+	if err != nil {
+		r.Error = awserr.New("SerializationError", "failed decoding EC2 Query response", err)
+		return
+	}
+}
+
+// UnmarshalMeta unmarshals response headers for the EC2 protocol.
+func UnmarshalMeta(r *request.Request) {
+	// TODO implement unmarshaling of request IDs
+}
+
+type xmlErrorResponse struct {
+	XMLName   xml.Name `xml:"Response"`
+	Code      string   `xml:"Errors>Error>Code"`
+	Message   string   `xml:"Errors>Error>Message"`
+	RequestID string   `xml:"RequestID"`
+}
+
+// UnmarshalError unmarshals a response error for the EC2 protocol.
+func UnmarshalError(r *request.Request) {
+	defer r.HTTPResponse.Body.Close()
+
+	resp := &xmlErrorResponse{}
+	err := xml.NewDecoder(r.HTTPResponse.Body).Decode(resp)
+	if err != nil && err != io.EOF {
+		r.Error = awserr.New("SerializationError", "failed decoding EC2 Query error response", err)
+	} else {
+		r.Error = awserr.NewRequestFailure(
+			awserr.New(resp.Code, resp.Message, nil),
+			r.HTTPResponse.StatusCode,
+			resp.RequestID,
+		)
+	}
+}
